1d9de97fb9c48d0a329b8d2e178b2b69e08f55f5
Nikita Agarwal <nikitaag206@gmail.com>
2022-07-08 12:25:03 -0700

    Added tap gesture for sign up

---
+++ StayFit/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  StayFit
+//
+//  Created by Nikita Agarwal on 7/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
