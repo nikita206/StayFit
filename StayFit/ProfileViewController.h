e2ac504e7b38461f7c77cfac1c942fcc26047fae
Nikita Agarwal <nikitaag206@gmail.com>
2022-07-11 12:39:41 -0700

    added logout functionality

---
+++ StayFit/ProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.h
+//  StayFit
+//
+//  Created by Nikita Agarwal on 7/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+- (IBAction)didTapLogout:(id)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StayFit/ProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.m
+//  StayFit
+//
+//  Created by Nikita Agarwal on 7/11/22.
+//
+
+#import "ProfileViewController.h"
+#import "Parse/Parse.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+                self.view.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"login"];
+    }];
+}
+@end
