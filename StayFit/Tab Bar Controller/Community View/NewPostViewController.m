14d0a7ec1a4837bafa9b2ddbbe3688af36473612
Nikita Agarwal <nikitaag206@gmail.com>
2022-07-26 15:51:01 -0700

    cleaned code, added comments, converted if-else blocks to switch

--- StayFit/Tab Bar Controller/Community View/NewPostViewController.m
+++ StayFit/Tab Bar Controller/Community View/NewPostViewController.m
@@ -21,61 +21,66 @@
 }
 
 - (IBAction)shareButton:(id)sender {
-    //takes the user back to the segment that was active after share button is clicked
-    if(self.segoutValue == 0){
-    if(self.image.image && ![self.caption.text isEqualToString:@""]){
-        [Post postUserImage:self.image.image withCaption:self.caption.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
-            if (error) {
-                NSLog(@"%s", "could not post");
+    //takes the user back to the segment that was active after share button is clicked (fitness/recipes)
+    switch(self.segoutValue){
+        //if fitness segment was active
+        case 0:
+            //checks if the caption or image is missing
+            if(self.image.image && ![self.caption.text isEqualToString:@""]){
+                [Post postUserImage:self.image.image withCaption:self.caption.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+                    if (error) {
+                        //throws an error if either is null
+                        NSLog(@"%s", "could not post");
+                    }
+                    else{
+                        //if neither of text fields is null then post is posted
+                        NSLog(@"Your post was posted!");
+                        [self.navigationController popViewControllerAnimated:YES];
+                    }
+                }];
             }
-            else{
-                NSLog(@"Awesome");
-                [self.navigationController popViewControllerAnimated:YES];
+        //if recipes segment was active
+        case 1:
+            //checks if the caption or image is missing
+            if(self.image.image && ![self.caption.text isEqualToString:@""]){
+                [recipesPost postUserImage:self.image.image withCaption:self.caption.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+                    if (error) {
+                        //throws an error if either is null
+                        NSLog(@"%s", "could not post");
+                    }
+                    else{
+                        //if neither of text fields is null then post is posted
+                        NSLog(@"Your post was posted!");
+                        [self.navigationController popViewControllerAnimated:YES];
+                    }
+                }];
             }
-        }];
-    }
-    }
-    else if(self.segoutValue == 1){
-    if(self.image.image && ![self.caption.text isEqualToString:@""]){
-        [recipesPost postUserImage:self.image.image withCaption:self.caption.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
-            if (error) {
-                NSLog(@"%s", "could not post");
-            }
-            else{
-                NSLog(@"Awesome");
-                [self.navigationController popViewControllerAnimated:YES];
-            }
-        }];
-    }
-    }
+        }
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
     // Get the image captured by the UIImagePickerController
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
     UIImage *resizedImage = [self resizeImage:originalImage withSize:CGSizeMake(500,500)];
     [self.image setImage:resizedImage];
-    
     // Dismisses UIImagePickerController to go back to your original view controller
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    //resizes the image
     UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
-    
     resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
     resizeImageView.image = image;
-    
     UIGraphicsBeginImageContext(size);
     [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
     return newImage;
 }
 
 - (IBAction)cameraRoll:(id)sender {
+    //enables user to pick a photo from their camera roll
     UIImagePickerController *imagePickerVC = [UIImagePickerController new];
     imagePickerVC.delegate = self;
         imagePickerVC.allowsEditing = YES;
@@ -85,18 +90,18 @@
 }
 
 - (IBAction)takePicture:(id)sender {
+    //enables user to open camera and take a picture
     UIImagePickerController *imagePickerVC = [UIImagePickerController new];
     imagePickerVC.delegate = self;
         imagePickerVC.allowsEditing = YES;
-
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
     }
     else {
+        //if camera is unavailable then camera roll is opened
         NSLog(@"Camera 🚫 available so we will use photo library instead");
         imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     }
-
     [self presentViewController:imagePickerVC animated:YES completion:nil];
 }
 @end
--- StayFit/Tab Bar Controller/Profile View/ProfileViewController.m
+++ StayFit/Tab Bar Controller/Profile View/ProfileViewController.m
@@ -15,34 +15,39 @@
 @implementation ProfileViewController
 
 - (void)viewDidLoad {
+    //sets the current user as the user logged in
     PFUser *user = [PFUser currentUser];
     [super viewDidLoad];
+    //sets user details on the basis of logged in user
     self.author.text = [NSString stringWithFormat:@"%@%@%@", user[@"firstName"]  , @" ", user[@"lastName"]];
     self.username.text = [NSString stringWithFormat:@"%@%@", @"@" , user.username];
     self.level.text = user[@"fitnessLevel"];
     self.profilePic.file = user[@"profileImage"];
     [self.profilePic loadInBackground];
+    //sets the radius of the profile pic of the user
     self.profilePic.layer.cornerRadius = self.profilePic.frame.size.width/2.1;
     self.profilePic.clipsToBounds = YES;
+    //ceates a tap gesture for profile pic
     UITapGestureRecognizer *profileTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapPhoto)];
     [self.profilePic addGestureRecognizer:profileTap];
     [self.profilePic setUserInteractionEnabled:YES];
 }
 
 - (IBAction)didTapLogout:(id)sender {
+    //logs out and segues to the login page
     [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-                self.view.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"login"];
+        self.view.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"login"];
     }];
 }
 
+//allows the user to change their profile picture using camera/camera roll
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
     // Get the image captured by the UIImagePickerController
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
     UIImage *editedImage = info[UIImagePickerControllerEditedImage];
     PFFileObject *profilePicture = [PFFileObject fileObjectWithName:@"image.png" data:UIImagePNGRepresentation(editedImage)];
-    
+    //sets the profile picture in Parse
     [[PFUser currentUser] setObject:profilePicture forKey:@"profileImage"];
         [[PFUser currentUser] saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
             if (succeeded) {
@@ -59,7 +64,8 @@
 }
 
 -(void) didTapPhoto {
-    NSLog(@"tippy tippy tap");
+    //opens Image Picker controller when tap gesture on profile picture is recognized
+    NSLog(@"The profile picture was tapped");
     UIImagePickerController *imagePickerVC = [UIImagePickerController new];
     imagePickerVC.delegate = self;
     imagePickerVC.allowsEditing = YES;
