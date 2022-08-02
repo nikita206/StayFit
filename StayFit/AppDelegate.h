7858acdd89c9a5cc67397598939e88db71c565e6
Nikita Agarwal <nikitaag206@gmail.com>
2022-08-02 15:54:14 -0700

    users are able to login while they are offline

--- StayFit/AppDelegate.h
+++ StayFit/AppDelegate.h
@@ -7,9 +7,10 @@
 
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UserNotifications.h>
-
+#import <CoreData/CoreData.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
-
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+- (void)saveContext;
 
 @end
 
